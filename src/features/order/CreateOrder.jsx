97902a5553839c7a57098c9bcf4f08fe2096ca85
Ashim rai <107597296+itsashim@users.noreply.github.com>
2024-01-23 23:35:32 +0545

    Error handling using action function and form data

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,13 +1,13 @@
 // import { useState } from "react";
 
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
-// const isValidPhone = (str) =>
-//   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-//     str
-//   );
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str
+  );
 
 const fakeCart = [
   {
@@ -37,6 +37,11 @@ function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
+
   return (
     <div>
       {/* <h2>Ready to order? Let's go!</h2> */}
@@ -52,6 +57,8 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -74,7 +81,7 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>Order now</button>
         </div>
       </Form>
     </div>
@@ -90,9 +97,15 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
-  const newOrder = await createOrder(cartData);
 
-  console.log(data);
+  const errors = {};
+  if (!isValidPhone(cartData.phone))
+    errors.phone = "Please give us your phone number";
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everthing is ok create  new order and redirect
+  const newOrder = await createOrder(cartData);
 
   return redirect(`/order/${newOrder.id}`);
 }
