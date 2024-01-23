67475b41d1e26e89037ac1dfba2495dad8706427
Ashim rai <107597296+itsashim@users.noreply.github.com>
2024-01-23 22:19:06 +0545

    Created object from Form from react route

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
+
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -26,7 +29,11 @@ const router = createBrowserRouter([
         errorElement: <Error />,
         loader: orderLoader,
       },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
     ],
   },
 ]);
