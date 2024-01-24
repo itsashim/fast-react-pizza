b1aad20723ab8c032b529703cbbdda30dcc539af
Ashim rai <107597296+itsashim@users.noreply.github.com>
2024-01-24 20:31:22 +0545

    Setup tailwind

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="text-3xl font-bold underline text-red-300">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
